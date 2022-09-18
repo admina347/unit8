7d9382ff900775a79f3a564eb261aa82cbe3015f
admina <it-mactep@mail.ru>
2022-09-18 10:28:11 +0500

    add try catch

--- Program.cs
+++ Program.cs
@@ -22,43 +22,39 @@
     {
         public static void Main(string[] args)
         {
-            //
             long dTotalSize = 0, fTotalSize = 0;
-            long totalSizeKb = 0, totalSizeMb = 0;
-
             string dirPath = "/home/admina/SFtest";
             Console.WriteLine("Введите путь к папке:");
-            //dirPath = Console.ReadLine();
+            dirPath = Console.ReadLine();
             Console.WriteLine("Указан путь: {0}", dirPath);
             DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
-            //Console.WriteLine("root: {0}", dirInfo.Root);
             //вложенные папки и файлы
-            var folders = dirInfo.GetDirectories();
-            if (!dirInfo.Exists)
+            try
             {
-                Console.WriteLine("Папка: {0} не существует!", dirPath);
+                var folders = dirInfo.GetDirectories();
+                if (!dirInfo.Exists)
+                {
+                    Console.WriteLine("Папка: {0} не существует!", dirPath);
+                }
+                else    //папка есть
+                {
+                    GetFileInfo(dirInfo, out fTotalSize);
+                    GetFolderInfo(folders, out dTotalSize);
+                    Console.WriteLine("Размер пакпки {0}: {1} байт", dirInfo.Name, dTotalSize + fTotalSize);
+                }
             }
-            else    //папка есть
+            catch (DirectoryNotFoundException dirEx)
             {
-                //
-                GetFileInfo(dirInfo, out fTotalSize);
-                //
-                GetFolderInfo(folders, out dTotalSize);
-                Console.WriteLine("Размер пакпки {0}: {1} байт", dirInfo.Name, dTotalSize + fTotalSize);
-                //totalSizeKb = (dTotalSize + fTotalSize) / 1024;
-                //totalSizeMb = totalSizeKb / 1024;
-                //Console.WriteLine("Размер пакпки {0}: {1} Mб", dirInfo.Name, totalSizeMb);
+                Console.WriteLine("Директория не найдена " + dirEx.Message);
             }
         }
         public static void GetFolderInfo(DirectoryInfo[] folders, out long dTotalSize)
         {
             long dSize = 0;
-            //Console.WriteLine("папки: ");
             foreach (var folder in folders)
             {
                 try
                 {
-                    //Console.WriteLine("{0} - {1}байт", folder.Name, DirectoryExtension.GetDirSize(folder));
                     dSize = dSize + DirectoryExtension.GetDirSize(folder);
                 }
                 catch (Exception ex)
@@ -70,12 +66,9 @@
         }
         public static void GetFileInfo(DirectoryInfo d, out long fTotalSize)
         {
-            //
             long fSize = 0;
-            //Console.WriteLine("Файлы: ");
-            foreach(var file in d.GetFiles())
+            foreach (var file in d.GetFiles())
             {
-                //Console.WriteLine("{0} - {1}байт", file.Name, file.Length);
                 fSize = fSize + file.Length;
             }
             fTotalSize = fSize;
